8d8214e61e332f867183f718b6f5e7fc61305812
S-John-S <classworkpc@gmail.com>
2017-05-05 14:12:04 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -20,7 +20,8 @@ echo "</pre>";
 ?>
 <div class="scroll-box" >	
 	<?php
-$pdb_file = "inputfile.pdb";
+	if ( $_FILES['file']['name']){
+	$pdb_file = "inputfile.pdb";
 	move_uploaded_file($_FILES['file']['tmp_name'], $pdb_file);
         $option = "-j";
 	passthru("pdb_std -i $pdb_file $option > $output 2>&1");
@@ -30,6 +31,8 @@ $pdb_file = "inputfile.pdb";
 	fclose($output_file);
         foreach ( $lines as $line ) {
         	echo $line . "<br/>";
+	}
+		echo "Error in command";
 	}
   ?>  
  </div> 
