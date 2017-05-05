326ca1e1fdc1b5a93460f73272b5c82720850aa9
S-John-S <classworkpc@gmail.com>
2017-05-05 13:32:17 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,22 +3,19 @@
 
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
-
 <?php
-$data = array();                // define array
-
-exec('ls -l', $data, $ret);     // execute command, output is array
-
-echo "<pre>";
-if ($ret == 0) {                // check status code. if successful
-    foreach ($data as $line) {  // process array line by line
-        echo "$line \n";
-    }
-} else {
-    echo "Error in command";    // if unsuccessful display error
-}
-echo "</pre>";
-?>
+$pdb_file = "inputfile.pdb";
+	move_uploaded_file($_FILES['file']['tmp_name'], $pdb_file);
+      
+	passthru("pdb_std -i $pdb_file $option > $output 2>&1");
+	$output_file = fopen(getcwd()."/".$output, "r");
+	$content = fread($output_file, filesize(getcwd()."/".$output));
+	$lines = explode("\n", $content);
+	fclose($output_file);
+        foreach ( $lines as $line ) {
+        	echo $line . "<br/>";
+	}
+  ?>  
   
 </body>
 </html> 
