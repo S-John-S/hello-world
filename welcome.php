dea8e02b97eb29a7bb2d7963338280899ea271c5
S-John-S <classworkpc@gmail.com>
2017-05-05 13:20:17 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -2,10 +2,22 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
 
-  <?php
-echo exec('ls -l');
+<?php
+$data = array();                // define array
+
+exec('ls -l', $data, $ret);     // execute command, output is array
+
+echo "<pre>";
+if ($ret == 0) {                // check status code. if successful
+    foreach ($data as $line) {  // process array line by line
+        echo "$line \n";
+    }
+} else {
+    echo "Error in command";    // if unsuccessful display error
+}
+echo "</pre>";
 ?>
   
 </body>
