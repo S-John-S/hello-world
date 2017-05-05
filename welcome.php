ce97618bb914c784d8d8cda18f399a6698ba7f3f
S-John-S <classworkpc@gmail.com>
2017-05-05 15:41:52 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -23,20 +23,28 @@ echo "</pre>";
 	
 	<?php
 	//if ( $_FILES['file']['name']){
-	$pdb_file = "inputfile.pdb";
+	//$pdb_file = "inputfile.pdb";
 	//move_uploaded_file($_FILES['file']['tmp_name'], $pdb_file);
-        $option = "-j";
+        //$option = "-j";
 	$output = array();
-	exec('pdb_std -i $pdb_file $option', $output, $return);
-	echo $output;
+	exec('pdb_std -i inputfile.pdb -j', $output, $return);
+	echo "<pre>";
+	if ($return == 0) {                // check status code. if successful
+	    foreach ($output as $lins) {  // process array line by line
+        	echo "$lins \n";
+    	}
+	} else {
+    	echo "Error in command";    // if unsuccessful display error
+	}
+	echo "</pre>";
 	//passthru("pdb_std -i $pdb_file $option > $output 2>&1");
-	$output_file = fopen(getcwd()."/".$output, "r");
-	$content = fread($output_file, filesize(getcwd()."/".$output));
-	$lines = explode("\n", $content);
-	fclose($output_file);
-        foreach ( $lines as $line ) {
-        	echo $line . "<br/>";
-	}//}
+	//$output_file = fopen(getcwd()."/".$output, "r");
+	//$content = fread($output_file, filesize(getcwd()."/".$output));
+	//$lines = explode("\n", $content);
+	//fclose($output_file);
+        //foreach ( $lines as $line ) {
+       // 	echo $line . "<br/>";
+	//}//}
 		echo "Error";
 	
   ?>   
