3d239449023f5d17485dee2cadab96fc4ea289e2
S-John-S <classworkpc@gmail.com>
2017-05-05 14:21:48 +0530

    Update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
  <html>
 <body>
 
-<form action="welcome.php" method="post">
+<form action="welcome.php" method="post" enctype="multipart/form-data">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 PDB: <input  type ="file" accept=".pdb" id="pdb_file" name ="file"><br>
