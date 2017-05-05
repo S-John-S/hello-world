d87bb53ce793b27c595c3bae2f0b1580d41fdf45
S-John-S <classworkpc@gmail.com>
2017-05-05 14:04:41 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,6 +4,22 @@
 Welcome <?php echo $_POST["name"]; ?><br>
 Your email address is: <?php echo $_POST["email"]; ?><br>
 <?php
+$data = array();                // define array
+
+exec('ls -l', $data, $ret);     // execute command, output is array
+
+echo "<pre>";
+if ($ret == 0) {                // check status code. if successful
+    foreach ($data as $line) {  // process array line by line
+        echo "$line \n";
+    }
+} else {
+    echo "Error in command";    // if unsuccessful display error
+}
+echo "</pre>";
+?>
+<div class="scroll-box" >	
+	<?php
 $pdb_file = "inputfile.pdb";
 	move_uploaded_file($_FILES['file']['tmp_name'], $pdb_file);
         $option = "-j";
@@ -16,6 +32,6 @@ $pdb_file = "inputfile.pdb";
         	echo $line . "<br/>";
 	}
   ?>  
-  
+ </div> 
 </body>
 </html> 
