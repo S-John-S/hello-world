6896323a3ec4b56e0f8dec0d3bcfb6a9d7ae362b
S-John-S <classworkpc@gmail.com>
2017-05-15 18:36:30 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -7,7 +7,7 @@ Your email address is: <?php echo $_POST["email"]; ?><br>
 $data = array();                // define array
 exec('chmod +x a.out');
 exec('./a.out',$hello,$re);
-	echo $hello;
+	echo $re;
 	
 exec('ls -l', $data, $ret);     // execute command, output is array
 
